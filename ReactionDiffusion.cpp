0b2b9efd3d86dc8fff84c5a10b7926c74a1f8b1e
jeremy <jeremysee_2@hotmail.com>
2022-03-12 22:47:28 +0000

    added prototypes

--- ReactionDiffusion.cpp
+++ ReactionDiffusion.cpp
@@ -7,26 +7,59 @@
 
 // }
 
-ReactionDiffusion::ReactionDiffusion () {
-
+ReactionDiffusion::ReactionDiffusion (double dt, int T, int Nx, int Ny, double a,
+                                      double b, double mu1, double mu2, double eps) {
+    ReactionDiffusion::dt   = dt;
+    ReactionDiffusion::T    = T;
+    ReactionDiffusion::Nx   = Nx;
+    ReactionDiffusion::Ny   = Ny;
+    ReactionDiffusion::a    = a;
+    ReactionDiffusion::b    = b;
+    ReactionDiffusion::mu1  = mu1;
+    ReactionDiffusion::mu2  = mu2;
+    ReactionDiffusion::eps  = eps;
 }
 
 ReactionDiffusion::~ReactionDiffusion() {
-
+    delete ReactionDiffusion::matrixU;
+    delete ReactionDiffusion::matrixV;
 }
 
 int ReactionDiffusion::SetInitialConditions() {
-
+    return 0;
 }
 
 int ReactionDiffusion::SetParameters() {
-
-}
-
-int ReactionDiffusion::SetParameters() {
-
+    // Allocate memory for the matrix
+    ReactionDiffusion::matrixU = new double[ReactionDiffusion::Nx*ReactionDiffusion::Ny];
+    ReactionDiffusion::matrixV = new double[ReactionDiffusion::Nx*ReactionDiffusion::Ny];
+    return 0;
 }
 
 int ReactionDiffusion::TimeIntegrate() {
+    // Iterate over u matrix
+    for (int i = 0; i < Nx; ++i) {
+        for (int j = 0; j < Ny; ++j) {
 
+        }
+    }
+
+    // Iterate over v matrix
+    for (int i = 0; i < Nx; ++i) {
+        for (int j = 0; j < Ny; ++j) {
+            
+        }
+    }
+    return 0;
+}
+
+double ReactionDiffusion::f1_(double u, double v) {
+    double res = ReactionDiffusion::eps*u*(1.0-u)*
+                 (u - (v+ReactionDiffusion::b)/ReactionDiffusion::a);
+    return res;
+}
+
+double ReactionDiffusion::f2_(double u, double v) {
+    double res = u*u*u - v;
+    return res;
 }
--- ReactionDiffusion.h
+++ ReactionDiffusion.h
@@ -3,14 +3,39 @@
 
 class ReactionDiffusion {
     private:
+        double f1_(double u, double v); /// Calculate parameter f1
+        double f2_(double u, double v); /// Calculate parameter f2
 
+        int Nx, Ny, T, dx, dy;
+        double dt, a, b, eps, mu1, mu2;
+        double* matrixU;
+        double* matrixV;
     public:
-        // Constructor
-        ReactionDiffusion();
-        // Destructor
+        /// Constructor
+        ReactionDiffusion(double dt, int T, int Nx, int Ny, double a,
+                          double b, double mu1, double mu2, double eps);
+        /// Destructor, free memory
         ~ReactionDiffusion();
+
+        /**
+         * @brief Set the Parameters required for the problem, initialise the 2D matrix.
+         * 
+         * @return int Error code.
+         */
         int SetParameters();
+
+        /**
+         * @brief Set the Initial Conditions of the problem
+         * 
+         * @return int Error code.
+         */
         int SetInitialConditions();
+
+        /**
+         * @brief Perform the full solution.
+         * 
+         * @return int Error code.
+         */
         int TimeIntegrate();
 };
 
