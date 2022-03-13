4abbb6a7492e4e7d8e88f1f406e23478a595d730
jeremy <jeremysee_2@hotmail.com>
2022-03-13 14:18:30 +0000

    reactiondiffusion method implementation

--- ReactionDiffusion.cpp
+++ ReactionDiffusion.cpp
@@ -1,65 +1,121 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include "ReactionDiffusion.h"
-#include "cblas.h"
 
-// #define F77NAME(x) x##_
-// extern "C" {
+#define RD ReactionDiffusion
 
-// }
-
-ReactionDiffusion::ReactionDiffusion (double dt, int T, int Nx, int Ny, double a,
-                                      double b, double mu1, double mu2, double eps) {
-    ReactionDiffusion::dt   = dt;
-    ReactionDiffusion::T    = T;
-    ReactionDiffusion::Nx   = Nx;
-    ReactionDiffusion::Ny   = Ny;
-    ReactionDiffusion::a    = a;
-    ReactionDiffusion::b    = b;
-    ReactionDiffusion::mu1  = mu1;
-    ReactionDiffusion::mu2  = mu2;
-    ReactionDiffusion::eps  = eps;
+RD::ReactionDiffusion  (double dt, int T, int Nx, int Ny, double a,
+                        double b, double mu1, double mu2, double eps,
+                        double dx, double dy) {
+    this->dt   = dt;
+    this->T    = T;
+    this->Nx   = Nx;
+    this->Ny   = Ny;
+    this->a    = a;
+    this->b    = b;
+    this->mu1  = mu1;
+    this->mu2  = mu2;
+    this->eps  = eps;
+    this->dx   = dx;
+    this->dy   = dy;
 }
 
-ReactionDiffusion::~ReactionDiffusion() {
-    delete ReactionDiffusion::matrixU;
-    delete ReactionDiffusion::matrixV;
+RD::~ReactionDiffusion() {
+    if(U1) delete U1;
+    if(U2) delete U2;
+    if(V1) delete V1;
+    if(V2) delete V2;
 }
 
-int ReactionDiffusion::SetInitialConditions() {
-    return 0;
-}
-
-int ReactionDiffusion::SetParameters() {
-    // Allocate memory for the matrix
-    ReactionDiffusion::matrixU = new double[ReactionDiffusion::Nx*ReactionDiffusion::Ny];
-    ReactionDiffusion::matrixV = new double[ReactionDiffusion::Nx*ReactionDiffusion::Ny];
-    return 0;
-}
-
-int ReactionDiffusion::TimeIntegrate() {
-    // Iterate over u matrix
-    for (int i = 0; i < Nx; ++i) {
-        for (int j = 0; j < Ny; ++j) {
-
+int RD::SetInitialConditions() {
+    // Set boundary conditions for u
+    for (int i = 0; i<Nx; ++i) {
+        for (int j = Ny/2+1; j<Ny; ++j) {
+            U1[j+Ny*i] = 1.0;
         }
     }
+
+    // Set boundary conditions for v
+    for (int i = 0; i<(Nx/2); ++i) {
+        for (int j = 0; j<Ny; ++j) {
+            V1[j+Ny*i] = a/2.0;
+        }
+    }
+    return 0;
+}
+
+int RD::SetParameters() {
+    // Allocate memory for the matrix
+    U1 = new double[Nx*Ny]();
+    U2 = new double[Nx*Ny]();
+    V1 = new double[Nx*Ny]();
+    V2 = new double[Nx*Ny]();
+    return 0;
+}
+
+int RD::TimeIntegrate() {
+    #pragma omp parallel for
+    for (int k = 0; k<int(T/dt); ++k) {
+        TimeIntegrateSingle();
+    }
+    return 0;
+}
+
+int RD::TimeIntegrateSingle() {
+    // Iterate over u matrix
+    double h = dx;
+    for (int i = 0; i < Nx; ++i) {
+        for (int j = 0; j < Ny; ++j) {
+            U2[j+Ny*i] = dt * (mu1 /(h*h) * (
+                                (i < Nx-1 ? U1[j+Ny*(i+1)] : 0) +
+                                (i        ? U1[j+Ny*(i-1)] : 0) +
+                                (j < Ny-1 ? U1[(j+1)+Ny*i] : 0) +
+                                (j        ? U1[(j-1)+Ny*i] : 0) -
+                                (4-(i==0)-(j==0)-(i==Nx-1)-(j==Ny-1))*U1[j+Ny*i]) +
+                                f1_(U1[j+Ny*i], V1[j+Ny*i])) + U1[j+Ny*i];
+        }
+    }
+
 
     // Iterate over v matrix
     for (int i = 0; i < Nx; ++i) {
         for (int j = 0; j < Ny; ++j) {
-            
+            V2[j+Ny*i] = dt * (mu1 /(h*h) * (
+                                (i < Nx-1 ? V1[j+Ny*(i+1)] : 0) +
+                                (i        ? V1[j+Ny*(i-1)] : 0) +
+                                (j < Ny-1 ? V1[(j+1)+Ny*i] : 0) +
+                                (j        ? V1[(j-1)+Ny*i] : 0) -
+                                (4-(i==0)-(j==0)-(i==Nx-1)-(j==Ny-1))*V1[j+Ny*i]) +
+                                f2_(U1[j+Ny*i], V1[j+Ny*i])) + V1[j+Ny*i];
+        }
+    }
+
+    // Save current time step for next iteration
+    for (int i = 0; i < Nx; ++i) {
+        for (int j = 0; j < Ny; ++j) {
+            U1[j+Ny*i] = U2[j+Ny*i];
+            V1[j+Ny*i] = V2[j+Ny*i];
         }
     }
     return 0;
 }
 
-double ReactionDiffusion::f1_(double u, double v) {
-    double res = ReactionDiffusion::eps*u*(1.0-u)*
-                 (u - (v+ReactionDiffusion::b)/ReactionDiffusion::a);
-    return res;
+void RD::writeOutput() {
+    std::ofstream outputFile("output.txt");
+    for (int j = 0; j < Ny; ++j) {
+        for (int i = 0; i < Nx; ++i) {
+            outputFile << std::setw(5) << i << std::setw(5) << j << std::setw(15) << U1[j + Ny * i] << std::setw(15) << V1[j + Ny * i] << std::endl;
+        }
+        outputFile << std::endl;
+    }
+    outputFile.close();
 }
 
-double ReactionDiffusion::f2_(double u, double v) {
-    double res = u*u*u - v;
-    return res;
+double RD::f1_(double u, double v) {
+    return double (eps*u*(1.0-u)* (u - (v+b)/a));
+}
+
+double RD::f2_(double u, double v) {
+    return double (u*u*u - v);
 }
--- ReactionDiffusion.h
+++ ReactionDiffusion.h
@@ -8,18 +8,30 @@ class ReactionDiffusion {
 
         int Nx, Ny, T, dx, dy;
         double dt, a, b, eps, mu1, mu2;
-        double* matrixU;
-        double* matrixV;
+        double* U1;
+        double* V1;
+        double* U2;
+        double* V2;
     public:
         /// Constructor
         ReactionDiffusion(double dt, int T, int Nx, int Ny, double a,
-                          double b, double mu1, double mu2, double eps);
+                          double b, double mu1, double mu2, double eps,
+                          double dx = 1, double dy = 1);
         /// Destructor, free memory
         ~ReactionDiffusion();
 
         /**
          * @brief Set the Parameters required for the problem, initialise the 2D matrix.
          * 
+         * @param dt    Time-step to use.
+         * @param T     Total integration time.
+         * @param Nx    Number of grid points in x
+         * @param Ny    Number of grid points in y
+         * @param a     Value of parameter a
+         * @param b     Value of parameter b
+         * @param mu1   Value of parameter mu1
+         * @param mu2   Value of parameter mu2
+         * @param eps   Value of parameter epsilon
          * @return int Error code.
          */
         int SetParameters();
@@ -37,6 +49,18 @@ class ReactionDiffusion {
          * @return int Error code.
          */
         int TimeIntegrate();
+
+        /**
+         * @brief Perform one time step of the integration.
+         * 
+         * @return int Error code.
+         */
+        int TimeIntegrateSingle();
+
+        /**
+         * @brief Getter method to output entire grid.
+         */
+        void writeOutput();
 };
 
 #endif
